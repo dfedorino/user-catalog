4e8af366dfd8abe241e1eba24cdb0b8c441ef5d7
Dmitry <dfedorino@gmail.com>
2021-05-18 16:25:36 +0800

    Remove redundant sql script
    Because redundant entity was created int the db.

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(250) NOT NULL,
-    family_name VARCHAR(250) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    address VARCHAR(250) NOT NULL,
-    phone_number VARCHAR(250) NOT NULL
-);
