cbd48e7311c579b247e1a0488ef123d5e5f7f10b
Dmitry <dfedorino@gmail.com>
2021-05-18 09:03:27 +0800

    Add resources for Spring Boot app

---
+++ src/main/java/com/dfedorino/user_catalog/UserCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.dfedorino.user_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCatalogApplication.class, args);
+	}
+
+}
