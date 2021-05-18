a6b81ccfe9a250cd8a78819d09ee8864bce86ee4
Dmitry <dfedorino@gmail.com>
2021-05-18 10:29:47 +0800

    Implement Base Entity class
    Field id it is not recommended to be exposed to any constructor or setter. It should be manipulated only by JPA implementation. BaseEntity class was implemented in order to provide the opportunity of inheriting the id field without exposing it to the concrete class constructor.

---
+++ src/main/java/com/dfedorino/user_catalog/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.dfedorino.user_catalog;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity                 // JPA annotation to make this object ready for storage in a JPA-based data store
+@Getter
+// Choose your inheritance strategy:
+//@Inheritance(strategy=InheritanceType.JOINED)
+//@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+//@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
+public abstract class BaseEntity {
+    @Id                 // JPA annotation to make this field a primary key
+    @GeneratedValue     // JPA annotation to make this field automatically populated with the JPA provider
+    private Long id;
+}
+
