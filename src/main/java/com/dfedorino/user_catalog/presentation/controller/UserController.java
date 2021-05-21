4873b7f40d545429769b344de770341baf0a3fbb
Dmitry <dfedorino@gmail.com>
2021-05-21 10:34:24 +0800

    Implement getAll and test

---
+++ src/main/java/com/dfedorino/user_catalog/presentation/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.dfedorino.user_catalog.presentation.controller;
+
+import com.dfedorino.user_catalog.application.UserRepository;
+import com.dfedorino.user_catalog.presentation.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepository repository;
+
+    UserController (UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/users")
+    List<User> all() {
+        List<User> all = new ArrayList<>();
+        repository.findAll().forEach(all::add);
+        return all;
+    }
+}
