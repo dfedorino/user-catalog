fbb0e3435af17b3445b8a33e33a2ef2b3ee85fba
Dmitry <dfedorino@gmail.com>
2021-05-21 13:14:49 +0800

    Implement put a single user
    - Implement put mapping;
    
    - Implement tests.

--- src/main/java/com/dfedorino/user_catalog/presentation/controller/UserController.java
+++ src/main/java/com/dfedorino/user_catalog/presentation/controller/UserController.java
@@ -6,6 +6,7 @@ import com.dfedorino.user_catalog.presentation.model.exception.UserNotFoundExcep
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,20 @@ public class UserController {
     @GetMapping("/users/{id}")
     User readUser(@PathVariable Long id) {
         return repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/users/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(user -> {
+                    user.setFirstName(newUser.getFirstName());
+                    user.setFamilyName(newUser.getFamilyName());
+                    user.setDateOfBirth(newUser.getDateOfBirth());
+                    user.setAddress(newUser.getAddress());
+                    user.setPhoneNumber(newUser.getPhoneNumber());
+                    return repository.save(user);
+                })
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
--- src/test/java/com/dfedorino/user_catalog/presentation/controller/UserControllerTest.java
+++ src/test/java/com/dfedorino/user_catalog/presentation/controller/UserControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.RequestBuilder;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,7 +30,12 @@ class UserControllerTest {
 
     @Test
     void testPostAll_FilledDatabase_ShouldReturnListWithAllUsersAndNewUser() throws Exception {
-        String user4Json = "{\"firstName\":\"first name 4\",\"familyName\":\"family name 4\",\"dateOfBirth\":\"1970-01-04\",\"address\":\"address 4\",\"phoneNumber\":\"number 4\"}";
+        String user4Json = "{" +
+                "\"firstName\":\"first name 4\"," +
+                "\"familyName\":\"family name 4\"," +
+                "\"dateOfBirth\":\"1970-01-04\"," +
+                "\"address\":\"address 4\"," +
+                "\"phoneNumber\":\"number 4\"}";
         RequestBuilder postRequest = post("/users")
                 .contentType(MediaType.APPLICATION_JSON )
                 .content(user4Json);
@@ -48,6 +54,39 @@ class UserControllerTest {
     @Test
     void testGetOne_GetNonExistingUserFromFilledDatabase_ShouldThrowException() throws Exception {
         this.mockMvc.perform(get("/users/999"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string("Could not find user 999"));
+    }
+
+    @Test
+    void testPutOne_UpdateExistingUserFromFilledDatabase_ShouldReturnUpdatedUser() throws Exception {
+        String user3Json = "{" +
+                "\"firstName\":\"first name 3\"," +
+                "\"familyName\":\"family name 3\"," +
+                "\"dateOfBirth\":\"1970-01-03\"," +
+                "\"address\":\"new address 3\"," +
+                "\"phoneNumber\":\"number 3\"}";
+        RequestBuilder putRequest = put("/users/3")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(user3Json);
+        this.mockMvc.perform(putRequest)
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testPutOne_UpdateNonExistingUserFromFilledDatabase_ShouldThrowException() throws Exception {
+        String user3Json = "{" +
+                "\"firstName\":\"first name 3\"," +
+                "\"familyName\":\"family name 3\"," +
+                "\"dateOfBirth\":\"1970-01-03\"," +
+                "\"address\":\"new address 3\"," +
+                "\"phoneNumber\":\"number 3\"}";
+        RequestBuilder putRequest = put("/users/999")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(user3Json);
+        this.mockMvc.perform(putRequest)
                 .andDo(print())
                 .andExpect(status().is4xxClientError())
                 .andExpect(content().string("Could not find user 999"));
