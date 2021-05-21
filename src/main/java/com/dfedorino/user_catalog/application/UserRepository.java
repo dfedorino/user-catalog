351e687d78d5214fc83ad7e169633e4fbb8d413f
Dmitry <dfedorino@gmail.com>
2021-05-21 08:27:03 +0800

    Refactor JpaRepository to CrudRepository
    Switch JpaRepository to CrudRepository to follow testing instructions in the other guide

--- src/main/java/com/dfedorino/user_catalog/application/UserRepository.java
+++ src/main/java/com/dfedorino/user_catalog/application/UserRepository.java
@@ -1,7 +1,9 @@
 package com.dfedorino.user_catalog.application;
 
 import com.dfedorino.user_catalog.presentation.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByFamilyName(String familyName);
+    void deleteByFamilyName(String familyName);
 }
