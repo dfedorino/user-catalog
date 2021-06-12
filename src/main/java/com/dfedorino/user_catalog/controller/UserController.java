7bd454706b9e15b9df7e9fac344228ba8ef4d7fd
Dmitry <dfedorino@gmail.com>
2021-06-12 21:46:59 +0800

    Add log4j2 logging API
    - Exclude Logback;
    
    - Add a sample logging line in the UserController.

--- src/main/java/com/dfedorino/user_catalog/controller/UserController.java
+++ src/main/java/com/dfedorino/user_catalog/controller/UserController.java
@@ -4,6 +4,7 @@ import com.dfedorino.user_catalog.repository.User;
 import com.dfedorino.user_catalog.repository.exception.UserAlreadyExistsException;
 import com.dfedorino.user_catalog.repository.exception.UserNotFoundException;
 import com.dfedorino.user_catalog.service.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Log4j2
 public class UserController {
     @Autowired
     private UserService service;
 
     @GetMapping("/users")
     List<User> all() {
+        log.debug("request to '/users'");
         return service.getAllUsers();
     }
 
