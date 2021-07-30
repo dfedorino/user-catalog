111e04d0ac3278408e99b02d0dee7954db1f14a3
Dmitry <dfedorino@gmail.com>
2021-07-30 08:53:03 +0800

    fix: refactor jwt filter

--- src/main/java/com/dfedorino/user_catalog/controller/JwtAuthorizationFilter.java
+++ src/main/java/com/dfedorino/user_catalog/controller/JwtAuthorizationFilter.java
@@ -6,9 +6,6 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
-import com.dfedorino.user_catalog.repository.exception.UserNotFoundException;
-import com.dfedorino.user_catalog.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -27,31 +24,43 @@ import java.util.stream.Collectors;
 
 @Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
-    @Autowired
-    UserService userService;
-
     private final Algorithm algorithm = Algorithm.HMAC256("secret");
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        try {
-            Map<String, Claim> claims;
-            if (!hasJwtToken(request) || (claims = verifyTokenAndGetClaims(request)) == null) {
-                SecurityContextHolder.clearContext();
-            } else {
-                String username = claims.get("sub").asString();
-                // throws exception if doesn't exist
-                boolean isExistingUser = userService.getUserByLogin(username) != null;
+//        try {
+//            Map<String, Claim> claims;
+//            if (!hasJwtToken(request) || (claims = verifyTokenAndGetClaims(request)) == null) {
+//                SecurityContextHolder.clearContext();
+//            } else {
+//                String username = claims.get("sub").asString();
+//                // throws exception if doesn't exist
+//                boolean isExistingUser = userService.getUserByLogin(username) != null;
+//                setUpSpringAuthentication(claims);
+//            }
+//            chain.doFilter(request, response);
+//        } catch (JWTVerificationException | UserNotFoundException e) {
+//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+//            response.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+//        }
+
+        if (hasJwtToken(request)) {
+            try {
+                System.out.println(">> JWT Token found, processing...");
+                Map<String, Claim> claims = verifyToken(request);
                 setUpSpringAuthentication(claims);
+                chain.doFilter(request, response);
+            } catch (JWTVerificationException e) {
+                System.out.println(">> Wrong jwt");
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
             }
-            chain.doFilter(request, response);
-        } catch (JWTVerificationException | UserNotFoundException e) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+        } else {
+            SecurityContextHolder.clearContext();
         }
+        chain.doFilter(request, response);
     }
 
-    private Map<String, Claim> verifyTokenAndGetClaims(HttpServletRequest request) throws JWTVerificationException {
+    private Map<String, Claim> verifyToken(HttpServletRequest request) throws JWTVerificationException {
         String jwtToken = request.getHeader(HttpHeaders.AUTHORIZATION).split(" ")[1];
         String issuer = "dfedorino.com";
         JWTVerifier verifier = JWT.require(algorithm)
@@ -68,15 +77,17 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         List<String> authorities = claims.get("authorities").asList(String.class);
 
         UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.get("subject"), null,
-                authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+                authorities.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList()));
         SecurityContextHolder.getContext().setAuthentication(auth);
 
     }
 
     private boolean hasJwtToken(HttpServletRequest request) {
         String authenticationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        String PREFIX = "Bearer ";
-        return authenticationHeader != null && authenticationHeader.startsWith(PREFIX);
+        String prefix = "Bearer ";
+        return authenticationHeader != null && authenticationHeader.startsWith(prefix);
     }
 
 }
